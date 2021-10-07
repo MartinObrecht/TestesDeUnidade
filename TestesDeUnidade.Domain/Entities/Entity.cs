a6105bd5521e4cc7d88a142e310c330a0dc769a9
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-10-07 11:58:20 -0300

    Add project files.

---
+++ TestesDeUnidade.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesDeUnidade.Domain.Entities
+{
+    public class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+    }
+}
---
+++ TestesDeUnidade.Domain/Enums/EOrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesDeUnidade.Domain.Enums
+{
+    public enum EOrderStatus
+    {
+        waitingPayment = 1,
+        waitingDelivery = 2,
+        canceled = 3,
+    }
+}
