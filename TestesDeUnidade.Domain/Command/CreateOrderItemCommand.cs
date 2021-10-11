b87263312633798fa38e76fd0b2737e8fab9455a
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-10-11 20:54:16 -0300

    Commands

---
+++ TestesDeUnidade.Domain/Command/CreateOrderItemCommand.cs
@@ -0,0 +1,34 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestesDeUnidade.Domain.Command.Interfaces;
+
+namespace TestesDeUnidade.Domain.Command
+{
+    public class CreateOrderItemCommand : Notifiable, ICommand
+    {
+        public CreateOrderItemCommand() { }
+
+        public CreateOrderItemCommand(Guid product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public Guid Product { get; set; }
+        public int Quantity { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasLen(Product.ToString(), 32, "Product", "Produto inválido")
+                .IsGreaterThan(Quantity, 0, "Quantity", "Quantidade inválida")
+            );
+        }
+    }
+}
---
+++ TestesDeUnidade.Domain/Command/Interfaces/ICommandResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesDeUnidade.Domain.Command.Interfaces
+{
+    public interface ICommandResult
+    {
+
+    }
+}
