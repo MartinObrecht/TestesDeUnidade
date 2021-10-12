a958a966109da3b312a4efe171f7bf2d54dc9734
Martin Obrecht <martin.obrecht@yahoo.com.br>
2021-10-12 09:01:22 -0300

    Handlers

---
+++ TestesDeUnidade.Domain/Handlers/Interfaces/IHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestesDeUnidade.Domain.Command.Interfaces;
+
+namespace TestesDeUnidade.Domain.Handlers.Interfaces
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
---
+++ TestesDeUnidade.Domain/Utils/ExtractGuids.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestesDeUnidade.Domain.Command;
+
+namespace TestesDeUnidade.Domain.Utils
+{
+    public static class ExtractGuids
+    {
+        public static IEnumerable<Guid> Extract(IList<CreateOrderItemCommand> items)
+        {
+            var guids = new List<Guid>();
+            foreach (var item in items)
+                guids.Add(item.Product);
+
+            return guids;
+        }
+    }
+}
